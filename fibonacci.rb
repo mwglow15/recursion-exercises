2536b8183522bae6b18bb74e363c80f454ca72a7
Matthew Glowacki <matthew.w.glowacki@gm.com>
2022-08-09 13:43:33 -0400

    Complete recursive fibonacci

--- fibonacci.rb
+++ fibonacci.rb
@@ -6,7 +6,6 @@ def fibonacci(num)
     elsif digit == 1
       result.push(1)
     else
-      puts digit
       sum = result[digit-2] + result[digit-1]
       result.push(sum)
     end
@@ -14,4 +13,17 @@ def fibonacci(num)
   result
 end
 
+def fibs_rec(num, result=[])
+  if num == 1
+    result.push(0)
+  elsif num == 2
+    result = fibs_rec(num-1)
+    result.push(1)
+  else
+    result = fibs_rec(num-1)
+    sum = result[num-3] + result[num-2]
+    result.push(sum)
+  end
+end
 p fibonacci(8)
+p fibs_rec(8)
